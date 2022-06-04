d6a2e09605215a4cf0cea491ae2d63d189b96eb2
CNEK1 <darkghostspain1@gmail.com>
2022-06-04 03:47:22 +0300

    Config. some args for CLI

---
+++ weatherIndex.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node 
+import {findArgs} from "./helpers/argsHelper.js";
+
+
+const initCli = () => {
+  const args = findArgs();
+  console.log(args);
+
+  if(args.s){
+
+  }
+  if(args.t){
+
+  }
+  if(args.h){
+      
+  }
+}
+initCli();
