e38003fc58ef709c8547495795ec4825440e9a0e
CNEK1 <darkghostspain1@gmail.com>
2022-06-04 19:31:02 +0300

    Options from yargs

--- helpers/argsHelper.js
+++ helpers/argsHelper.js
@@ -1,8 +1,23 @@
 import yargs from "yargs";
 
 const findArgs = () => {
-   const args = yargs(process.argv.slice(2)).argv;
-   return args;
+const y = yargs()
+y.version('1.0.0')
+
+// --- ADD OPTIONS ----
+return y.options('s', {
+  alias: "search",
+  required: false,  
+  describe: "Weather in seatch town",
+  type: "string",
+})
+.options('t', {
+  alias: "token",
+  required: false,
+  describe: "Saving our token",
+  type: "string",
+})
+
 }
 
 
